db8c677ae3f73280bdd6dbfad9cd8bc742fb5116
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-19 19:20:40 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      const newId = data.id;
+    
+      document.body.append(newId)
+      
+    })
+   
+    .catch(function (error) {
+        const errorMessage = error.message
+        document.body.append(errorMessage)
+        console.log(error.message);
+      });
+
+
+
+  }
+  
